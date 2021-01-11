2e7319ecaa7100574b28a933b39e1b65aa12092e
jedgodsey <jed.godsey@gmail.com>
2021-01-10 20:49:53 -0800

    filled out passportjs file

--- walk-it-out-back-end-master/index.js
+++ walk-it-out-back-end-master/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const parser = require('body-parser')
 const cors = require('cors')
 
+const passport = require('./config/passport')()
+
 const dogController = require('./controllers/dogs.js')
 
 const app = express()
 
 app.use(cors())
 app.use(parser.json())
+app.use(passport.initialize())
 
 app.use('/api/dogs', dogController)
 
